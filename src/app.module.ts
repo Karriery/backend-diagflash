cb32ad8867dc4085edf4ec06d0b339169637f1e9
halim-boussada <halimboussada10@gmail.com>
2024-01-24 15:41:24 +0100

    cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { MeetingModule } from './meeting/meeting.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      "mongodb://localhost:27017/diagflash"
+      "mongodb+srv://halim:20028952Sami@cluster0.b1pz3.mongodb.net/halim?retryWrites=true&w=majority"
     ),
     AuthModule,
     DocumentModule,
